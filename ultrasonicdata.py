bec34d7f5e3b0d0426540bf37813364abee69e51
diegoaga2024 <122946020+diegoaga2024@users.noreply.github.com>
2023-04-20 16:59:43 -0700

    Update ultrasonicdata.py

--- ultrasonicdata.py
+++ ultrasonicdata.py
@@ -1,77 +1,67 @@
+# Import required libraries
 import time
 import matplotlib.pyplot as plt
-
-# Import SPI library (for hardware SPI) and MCP3008 library.
+import numpy as np
+import RPi.GPIO as GPIO
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 
-import RPi.GPIO as GPIO
+# Set up GPIO pins
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(13, GPIO.IN)
-GPIO.setup(11, GPIO.IN)
-
+GPIO.setup(11, GPIO.IN)  # Button
+GPIO.setup(13, GPIO.IN)  # Ultrasonic sensor
 GPIO.setwarnings(False)
 
-#Hardware SPI configuration
-SPI_PORT= 0
-SPI_DEVICE=0
-mcp= Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+# Set up SPI connection to MCP3008 ADC
+SPI_PORT = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
-# Create lists to store distance and time data
-distances = []
-times = []
+# Set up variables for plotting
+max_distance = 5  # Max distance to plot (in inches)
+max_time = 5  # Max time to plot (in seconds)
+num_data_points = 100  # Number of data points to plot
+time_array = np.linspace(0, max_time, num_data_points)
+distance_array = np.zeros(num_data_points)
 
-# Main program loop.
+# Main program loop
 while True:
+    if GPIO.input(11):  # Button is pressed
+        print("Button pressed, recording data...")
+        start_time = time.time()  # Record start time
 
-    # Check if button on pin 11 is pressed
-    if GPIO.input(11):
-        # Clear lists
-        distances = []
-        times = []
-        start_time = time.time()
-        # Wait for 5 seconds to collect data
-        while time.time() - start_time < 5:
+        # Collect data for 5 seconds
+        for i in range(num_data_points):
             # Read distance from ultrasonic sensor
-            distance = # Code to read distance from ultrasonic sensor
-            # Append distance and time to lists
-            distances.append(distance)
-            times.append(time.time() - start_time)
-            time.sleep(0.1)
-        # Plot data if we have valid data
-        if distances and times:
-            # Filter out distances greater than 5 inches
-            distances = [d if d <= 5 else None for d in distances]
-            # Filter out times greater than 5 seconds
-            times = [t if t <= 5 else None for t in times]
-            # Remove None values
-            distances = [d for d in distances if d is not None]
-            times = [t for t in times if t is not None]
-            if distances and times:
-                # Plot distance vs time
-                plt.plot(times, distances)
-                plt.xlabel('Time (s)')
-                plt.ylabel('Distance (in)')
-                plt.show()
+            GPIO.setup(13, GPIO.OUT)
+            GPIO.output(13, True)
+            time.sleep(0.00001)
+            GPIO.output(13, False)
+            start_pulse = time.time()
+            stop_pulse = time.time()
+            while GPIO.input(13) == 0:
+                start_pulse = time.time()
+            while GPIO.input(13) == 1:
+                stop_pulse = time.time()
+            pulse_duration = stop_pulse - start_pulse
+            distance = pulse_duration * 17150
+            distance = round(distance, 2)
+            if distance > max_distance:
+                distance = max_distance
+            distance_array[i] = distance
+
+            # Wait for 0.05 seconds before reading again
+            time.sleep(0.05)
+
+        # Plot distance vs time
+        if time.time() - start_time < max_time:
+            print("Data not valid yet")
         else:
-            print('Data not valid yet')
-    else:
-        # Code to plot distance, velocity, and acceleration
-        # Read distance from ultrasonic sensor
-        distance = # Code to read distance from ultrasonic sensor
-        # Append distance and time to lists
-        distances.append(distance)
-        times.append(time.time())
-        # Filter out distances greater than 5 inches
-        distances = [d if d <= 5 else None for d in distances]
-        # Filter out times greater than 5 seconds
-        times = [t - times[0] if t - times[0] <= 5 else None for t in times]
-        # Remove None values
-        distances = [d for d in distances if d is not None]
-        times = [t for t in times if t is not None]
-        if distances and times:
-            # Plot distance vs time
-            plt.plot(times, distances)
-            plt.xlabel('Time (s)')
-            plt.ylabel('Distance (in)')
+            fig, ax = plt.subplots()
+            ax.plot(time_array, distance_array)
+            ax.set_xlim([0, max_time])
+            ax.set_ylim([0, max_distance])
+            ax.set_xlabel('Time (s)')
+            ax.set_ylabel('Distance (in)')
+            ax.set_title('Ultrasonic Distance vs Time')
             plt.show()

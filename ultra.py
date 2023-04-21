9850851e63b24614f3c6158ebcb88a9abf56dae2
diegoaga2024 <122946020+diegoaga2024@users.noreply.github.com>
2023-04-20 17:40:24 -0700

    Update ultra.py

--- ultra.py
+++ ultra.py
@@ -1,5 +1,6 @@
 import grovepi
 import time
+import matplotlib.pyplot as plot
 
 # set I2C to use the hardware bus
 grovepi.set_bus("RPI_1")
@@ -7,6 +8,15 @@ grovepi.set_bus("RPI_1")
 # Connect the Grove Ultrasonic Ranger to digital port D4
 # SIG,NC,VCC,GND
 ultrasonic_ranger = 4
+
+# Connect the Grove Button to digital port D3
+# SIG,NC,VCC,GND
+button = 3
+
+grovepi.pinMode(button,"INPUT")
+
+threshold=10;
+
 
 while True:
     try:
@@ -16,4 +26,18 @@ while True:
     except Exception as e:
         print ("Error:{}".format(e))
     
+    if (grovepi.ultrasonicRead(ultrasonic_ranger) > threshold):
+        print(" Far enough")
+        print(grovepi.digitalRead(button))
+        time.sleep(.5)
+        
+        except IOError:
+            print ("Error")
+        
     time.sleep(0.1) # don't overload the i2c bus
+    
+    else:
+        print("Back off")
+
+        
+        

54868d5b0369a028fa85b14fceb1103492997a53
diegoaga2024 <122946020+diegoaga2024@users.noreply.github.com>
2023-04-21 17:36:10 -0700

    Update ultra.py

--- ultra.py
+++ ultra.py
@@ -27,28 +27,33 @@ time_list = []
 value_to_remove = 65535
 garbage_indices = []
 
+startflag=0
+
 while True:  
-   act_dist = int(grovepi.ultrasonicRead(ultrasonic_ranger))
    
-   if ((grovepi.digitalRead(button))):# Button is pressed
+   while value_to_remove in dist_list:
+      index = dist_list.index(value_to_remove) #get next 
+      time_list.pop(index)
+      dist_list.remove(value_to_remove)
+      
+   print(dist_list) 
+   print(time_list)
+   print("Size of the list:", len(dist_list))
+   
+   if ((grovepi.digitalRead(button)) and (startflag==0)):# Button is pressed, hasn't started
        # get the current time and add it to the list
          print("Button pressed, recording data...")
-         start_time = time.time()
-         time_list.append(0)
-         if ((act_dist > lower_thresh) and (act_dist < upper_thresh)):  #within threshold
-           # Read distance value from Ultrasonic
-            dist_list.append(int(grovepi.ultrasonicRead(ultrasonic_ranger)))
-            # get the current time and add it to the list
-            time_list.append(time.time()-start_time)
-   else:
-         while value_to_remove in dist_list:
-            index = dist_list.index(value_to_remove) #get next 
-            time_list.pop(index)
-            dist_list.remove(value_to_remove)
-
-         # print the formatted time values  
-         print(dist_list) 
-         print(time_list)
-         print("Size of the list:", len(dist_list))
+         time_list.append(time.time()-start_time)
+         startflag=1
+         
+   else if ((grovepi.digitalRead(button)) and (startflag==1)):
+      if ((act_dist > lower_thresh) and (act_dist < upper_thresh) and (startflag==1)):  #within threshold
+         # Read distance value from Ultrasonic
+         dist_list.append(int(grovepi.ultrasonicRead(ultrasonic_ranger)))
+         # get the current time and add it to the list
+         time_list.append(time.time()-start_time)
+         
+      else if ((act_dist < lower_thresh) or (act_dist < upper_thresh)):
+             print("Stay in Range")
 
         

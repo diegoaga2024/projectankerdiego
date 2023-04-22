c6ca536ccdfd911dcd6d0ee4a5a7b5cfb5943b42
diegoaga2024 <122946020+diegoaga2024@users.noreply.github.com>
2023-04-21 17:13:05 -0700

    Update ultra.py

--- ultra.py
+++ ultra.py
@@ -30,17 +30,21 @@ garbage_indices = []
 while True:  
    act_dist = int(grovepi.ultrasonicRead(ultrasonic_ranger))
    
-   if ((grovepi.digitalRead(button)) and (act_dist > lower_thresh) and (act_dist < upper_thresh)):  # Button is pressed
-    print("Button pressed, recording data...")
-    # Read distance value from Ultrasonic
-    act_dist = int(grovepi.ultrasonicRead(ultrasonic_ranger))
-    dist_list.append(act_dist)
-    time.sleep(0.1) # don't overload the i2c bus
-    # get the current time and add it to the list
-    current_time = time.time()
-    time_list.append(current_time)
-    # wait for 1 second
-    time.sleep(1)
+   if ((grovepi.digitalRead(button)):# Button is pressed
+       # get the current time and add it to the list
+         start_time = time.time()
+         time_list.append(start_time)
+       if ((act_dist > lower_thresh) and (act_dist < upper_thresh)):  #within threshold
+         print("Button pressed, recording data...")
+         # Read distance value from Ultrasonic
+         act_dist = int(grovepi.ultrasonicRead(ultrasonic_ranger))
+         dist_list.append(act_dist)
+         time.sleep(0.1) # don't overload the i2c bus
+         # get the current time and add it to the list
+         current_time = time.time()
+         time_list.append(current_time)
+         # wait for 1 second
+         time.sleep(1)
      
 
    else:
@@ -48,7 +52,12 @@ while True:
          index = dist_list.index(value_to_remove)
          time_list.pop(index)
          dist_list.remove(value_to_remove)
-         
+      
+      # format the time values
+      formatted_times = [round(t - start_time, 3) for t in time_list]
+
+      # print the formatted time values
+      print(formatted_times)   
       print(dist_list) 
       print(time_list)
       print("Size of the list:", len(dist_list))

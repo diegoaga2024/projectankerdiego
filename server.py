87f76124be9fa28d59c417cfc14e353b71f85cef
Ankur Mathur <ankur.mathur512@gmail.com>
2023-04-25 13:02:25 -0700

    adds code

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 import paho.mqtt.subscribe as subscribe
-dist = subscribe.simple('distance', hostname="eclipse.usc.edu", port=11000)
-vel = subscribe.simple('velocity', hostname="eclipse.usc.edu", port=11001)
-acc = subscribe.simple('acceleration', hostname="eclipse.usc.edu", port=11002)
+dist_list = subscribe.simple('distance', hostname="eclipse.usc.edu", port=11000)
+vel_list = subscribe.simple('velocity', hostname="eclipse.usc.edu", port=11001)
+acc_list = subscribe.simple('acceleration', hostname="eclipse.usc.edu", port=11002)
 
-print((dist.topic, dist.payload))
-print((vel.topic, vel.payload))
-print((acc.topic, acc.payload))
+print((dist_list.topic, dist_list.payload))
+print((vel_list.topic, vel_list.payload))
+print((acc_list.topic, acc_list.payload))

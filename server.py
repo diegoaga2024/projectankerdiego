fa039437aea0ab82e7f769a170badbbe956108dd
diegoaga2024 <122946020+diegoaga2024@users.noreply.github.com>
2023-04-27 13:09:21 -0700

    Update server.py

--- server.py
+++ server.py
@@ -49,22 +49,19 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message_from_time(client, userdata, message):
     print("Received time list: "+ message.payload.decode())
-    time_samples= message.payload.decode()
-    print(type(time_samples))
+    time_samples= time_samples_list = [float(x) for x in message.payload.decode().split()]
         
 def on_message_from_dist(client, userdata, message):
     print("Received dist list: "+ message.payload.decode())
-    dist_samples= message.payload.decode()
+    dist_samples= dist_samples_list = [float(x) for x in message.payload.decode().split()]
 
 def on_message_from_vel(client, userdata, message):
     print("Received velocity list: " + message.payload.decode())
-    vel_samples= message.payload.decode()
+    vel_samples= vel_samples_list = [float(x) for x in message.payload.decode().split()]
 
 def on_message_from_acc(client, userdata, message):
     print("Received acceleration list: "+ message.payload.decode())
-    acc_samples= message.payload.decode()
-
-
+    acc_samples= acc_samples_list = [float(x) for x in message.payload.decode().split()]
 
 client= mqtt.Client()    #create a client object
 client.on_connect = on_connect #attach the on_connect() callback function defined above to the mqtt client

a37eaf8c0702bb86b1e02eee0bf4a3041835bc7d
Ankur Mathur <ankur.mathur512@gmail.com>
2023-04-27 14:32:32 -0700

    asd

--- server.py
+++ server.py
@@ -8,31 +8,31 @@ dist_samples= []
 vel_samples= []
 acc_samples= []
 
-gotdata=0
 
-def get_plot():
-        #fig, axs = plot.subplots(3, 1, figsize=(6, 8))
+def get_plot(t, d, v, a):
+        fig, axs = plot.subplots(3, 1, figsize=(6, 8))
         # Adjust the spacing between subplots
         plot.subplots_adjust(hspace=0.5)
        # Plot the distance vs. time data
-        axs[0].plot(time_samples, dist_samples, 'b-')
+        axs[0].plot(t, d, 'b-')
         axs[0].set_xlabel('Time (s)')
         axs[0].set_ylabel('Distance (m)')
         axs[0].set_title('Distance vs. Time')
 
         # Plot the velocity vs. time data
-        axs[1].plot(time_samples[:-1], vel_samples, 'g-')
+        axs[1].plot(t[:-1], v, 'g-')
         axs[1].set_xlabel('Time (s)')
         axs[1].set_ylabel('Velocity (m/s)')
         axs[1].set_title('Velocity vs. Time')
 
          # Plot the acceleration vs. time data
-        axs[2].plot(time_samples[:-2], acc_samples, 'r-')
+        axs[2].plot(t[:-2], a, 'r-')
         axs[2].set_xlabel('Time (s)')
         axs[2].set_ylabel('Acceleration (m/s^2)')
         axs[2].set_title('Acceleration vs. Time')
 
         # Save the plot to a file
+        print('saving plot')
         plot.savefig('plots.jpg')
 
 def on_connect(client, userdata, flags, rc):
@@ -41,41 +41,50 @@ def on_connect(client, userdata, flags, rc):
         client.subscribe("diegoankur/dist")
         client.subscribe("diegoankur/vel")
         client.subscribe("diegoankur/acc")
-        
+
         client.message_callback_add("diegoankur/time", on_message_from_time)
         client.message_callback_add("diegoankur/dist", on_message_from_dist)
         client.message_callback_add("diegoankur/vel", on_message_from_vel)
         client.message_callback_add("diegoankur/acc", on_message_from_acc)
-        
-        if (gotdata):
-                get_plot()
+
+        return time_samples
+        return dist_samples
+        return vel_samples
+        return acc_samples
+
 
 def on_message_from_time(client, userdata, message):
     print("Received time list: ")
     time_samples= [float(x) for x in message.payload.decode()[1:-1].split(", ")]
     print(time_samples)
+    return time_samples
         
 def on_message_from_dist(client, userdata, message):
     print("Received dist list: ")
     dist_samples= [float(x) for x in message.payload.decode()[1:-1].split(", ")]
     print(dist_samples)
+    return dist_samples
 
 def on_message_from_vel(client, userdata, message):
     print("Received velocity list: ")
     vel_samples= [float(x) for x in message.payload.decode()[1:-1].split(", ")]
     print(vel_samples)
+    return vel_samples
 
 def on_message_from_acc(client, userdata, message):
     print("Received acceleration list: ")
     acc_samples= [float(x) for x in message.payload.decode()[1:-1].split(", ")]
     print(acc_samples)
-    gotdata=1
+    return acc_samples
 
 while True:
         client= mqtt.Client()    #create a client object
         client.on_connect = on_connect #attach the on_connect() callback function defined above to the mqtt client
         client.connect("mqtt.eclipseprojects.io", 1883, 60) # Connect using the following hostname, port, and keepalive
         time.sleep(1)
-        client.loop_forever()
+        client.loop_start()
+        print(acc_samples)
+        if bool(acc_samples):
+            get_plot(time_samples, dist_samples, vel_samples, acc_samples)
 
 
